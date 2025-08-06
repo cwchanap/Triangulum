13129d1e0628eb3e23cbfd56144e31dcb2ca4819
cwchanap <cwchanap@connect.ust.hk>
2025-08-05 23:35:15 -0700

    Initial Commit

---
+++ TriangulumTests/TriangulumTests.swift
@@ -0,0 +1,17 @@
+//
+//  TriangulumTests.swift
+//  TriangulumTests
+//
+//  Created by Chan Wai Chan on 5/8/2025.
+//
+
+import Testing
+@testable import Triangulum
+
+struct TriangulumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
